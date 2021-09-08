a0aa416c5ac667a9d35a1b24752777f81f43c14a
Gon Banchero <gonbanchero@Gons-iMac.local>
2021-09-08 14:40:11 -0300

    burger menu js

---
+++ burger.js
@@ -0,0 +1,21 @@
+const menuBurger = document.querySelector('.menu')
+console.log(menuBurger)
+const btnBurger = document.querySelector('.btn-burger')
+console.log(btnBurger)
+const imgBurger = document.querySelector('.btn-burger img')
+console.log(imgBurger)
+
+btnBurger.addEventListener('click', mostrarOcultarBurger);
+
+function mostrarOcultarBurger () {
+    if (menuBurger.classList.contains('hide') ) {
+        menuBurger.classList.add('active')
+        menuBurger.classList.remove('hide')
+        imgBurger.src="img/menu-active.svg";
+    } else {
+        menuBurger.classList.remove('active')
+        menuBurger.classList.add('hide')
+        imgBurger.src="img/menu-inactive.svg";
+    }
+    
+}
